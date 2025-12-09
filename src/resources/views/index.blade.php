5e6ac14420adebc392282a1bb32075a8b91faf28
ikemi yuki <miilyu237@outlook.jp>
2025-12-09 17:17:53 +0900

    フォーム入力ページ、入力確認ページ、お問い合わせ完了ページの作成

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.app')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
+@endsection
+
+@section('content')
+    <div class="contact-form__content">
+        <div class="contact-form__header">
+            <h2>お問い合わせ</h2>
+        </div>
+        <form class="form" action="/contacts/confirm" method="post">
+            <div class="form__group">
+                <div class="form__group-title">
+                    <span class="form__label--item">お名前</span>
+                    <span class="form__label--required">必須</span>
+                </div>
+                <div class="form__group-content">
+                    <input class="form__input" type="text" name="name" placeholder="テスト太郎">
+                    <div class="form__error">
+                        バリデーション
+                    </div>
+                </div>
+            </div>
+            <div class="form__group">
+                <div class="form__group-title">
+                    <span class="form__label--item">メールアドレス</span>
+                    <span class="form__label--required">必須</span>
+                </div>
+                <div class="form__group-content">
+                    <input class="form__input" type="mail" name="email" placeholder="test@example.com">
+                    <div class="form__error">
+                        バリデーション
+                    </div>
+                </div>
+            </div>
+            <div class="form__group">
+                <div class="form__group-title">
+                    <span class="form__label--item">電話番号</span>
+                    <span class="form__label--required">必須</span>
+                </div>
+                <div class="form__group-content">
+                    <input class="form__input" type="tel" name="tel" placeholder="09012345678">
+                    <div class="form__error">
+                        バリデーション
+                    </div>
+                </div>
+            </div>
+            <div class="form__group">
+                <div class="form__label">お問い合わせ内容</div>
+                <div class="form__group-content">
+                    <textarea class="form__text" name="content" placeholder="資料をいただきたいです"></textarea>
+                </div>
+            </div>
+            <div class="form__button">
+                <button class="form__button-submit" type="submit">送信</button>
+            </div>
+        </form>
+    </div>
+@endsection
